bccfca089ce4fb5d50c82d11dc5048dbfe2d4111
David J Birnbaum <djbpitt@gmail.com>
2018-07-12 10:09:58 -0400

    clean up python_string directory

--- python_string/python_string.py
+++ python_string/python_string.py
@@ -1,20 +1,20 @@
 from xml.dom.pulldom import CHARACTERS, START_ELEMENT, parseString, END_ELEMENT
 
 output = []
-with open('flattened.xml') as input:
+with open('../input/basic/flattened.xml') as input:
     for event, node in parseString(input.read()):
         if event == START_ELEMENT:
-            if node.hasAttribute('th:eID'):    
+            if node.hasAttribute('th:eID'): # Trojan end tag  
                 output.append('</')
-            else: # Trojan start tags and non-Trojan
+            else: # Trojan start tags and non-Trojan elements
                 output.append('<')
             output.append(node.nodeName)
-            for attname, attvalue in node.attributes.items():
-                if not(attname.startswith('th:')):
+            for attname, attvalue in node.attributes.items(): # remove Trojan attributes and namespace declaration
+                if not (attname.startswith('th:') or attname == 'xmlns:th'):
                     output.append(' ' + attname + '="' + attvalue + '"')
             output.append('>')
-        if event == END_ELEMENT:
-            if not node.hasAttribute('th:sID') and not node.hasAttribute('th:eID'):
+        if event == END_ELEMENT: 
+            if not (node.hasAttribute('th:sID') or node.hasAttribute('th:eID')): # non-Trojan only
                 output.append('</' + node.localName + '>')
         elif event == CHARACTERS:
             output.append(node.data)
