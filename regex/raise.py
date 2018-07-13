1c43ac498aa186c760de4940d1cb74b51f1f4537
David J Birnbaum <djbpitt@gmail.com>
2018-07-13 16:16:52 -0400

    interim commit of paper

--- regex/raise.py
+++ regex/raise.py
@@ -1,7 +1,7 @@
 import sys
 import re
 
-start = re.compile(r'(<[^>]+?)th:sID\s*?=\s*[\'"]\w+?[\'"](.*?)\/(>)', re.S)
+start = re.compile(r'(<[^>]+?)th:sID\s*=\s*[\'"]\w+?[\'"](.*?)\/(>)', re.S)
 end =   re.compile(r'(<)(\S+?)\s+[^>]*?th:eID=[\'"]\w+[\'"][^>]*?\/(>)', re.S)
 
 with open(sys.argv[1], 'r') if len(sys.argv) > 1 else sys.stdin as f:
