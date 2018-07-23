37a552a7e38321efc7e3deac7d1def4ae12cc0d8
C. M. Sperberg-McQueen <cmsmcq+github@blackmesatech.com>
2018-07-22 20:57:31 -0600

    Use SAXON_HOME

--- inside-out/raise_he.sh
+++ inside-out/raise_he.sh
@@ -26,7 +26,7 @@ for i do
 done
     	
 
-SAX="${SAX_HOME:-/opt/local/bin/saxon-he/saxon9he.jar}"
+SAX="${SAXON_HOME:-/opt/local/bin/saxon-he/saxon9he.jar}"
 
 ### echo /usr/bin/time -l /usr/bin/java $JOPTS -jar $SAX -l -s:$1 -xsl:raise.xsl -o:$2 $SOPTS
 /usr/bin/java $JOPTS -jar $SAX -l -s:$1 -xsl:raise.xsl -o:$2 $SOPTS
